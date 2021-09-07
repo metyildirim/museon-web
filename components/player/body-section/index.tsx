1740eb2905a612430bf909f43a9bf8fd6e0a7b1f
Mehmet Emin Yıldırım <mehmetcts@gmail.com>
2021-09-08 00:05:37 +0300

    add route history controls

--- components/player/body-section/index.tsx
+++ components/player/body-section/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
@@ -15,16 +15,83 @@ type BodySectionProps = {
   children: JSX.Element;
 };
 
+enum RouteActions {
+  NEW_ROUTE,
+  BACK,
+  FORWARD,
+}
+
+const routeStack: Array<string> = [];
+let routeAction = RouteActions.NEW_ROUTE;
+let routeIndex = 0;
+let isInitialPath = true;
+let shouldClearHistory = false;
+
 const BodySection = ({ children }: BodySectionProps) => {
   const router = useRouter();
-  const [disabledHome, setDisabledHome] = useState(false);
+  const [disabledHome, setDisabledHome] = useState(true);
   const [disabledBack, setDisabledBack] = useState(true);
   const [disabledForward, setDisabledForward] = useState(true);
+
   const onHomeClicked = () => {
     router.push("/player/home", undefined, { shallow: true });
+    setDisabledHome(true);
   };
-  const onBackClicked = () => {};
-  const onForwardClicked = () => {};
+
+  const onBackClicked = () => {
+    routeAction = RouteActions.BACK;
+    shouldClearHistory = true;
+    router.push(routeStack[routeIndex - 1], undefined, { shallow: true });
+  };
+
+  const onForwardClicked = () => {
+    routeAction = RouteActions.FORWARD;
+    router.push(routeStack[routeIndex + 1], undefined, { shallow: true });
+  };
+
+  const onRouteChanged = (url: string) => {
+    if (routeAction === RouteActions.NEW_ROUTE) {
+      if (shouldClearHistory) {
+        routeStack.splice(routeIndex + 1);
+        setDisabledForward(true);
+        shouldClearHistory = false;
+      }
+      if (!isInitialPath) {
+        setDisabledBack(false);
+        routeIndex += 1;
+      } else {
+        isInitialPath = false;
+      }
+      routeStack.push(url);
+    } else if (routeAction === RouteActions.BACK) {
+      routeIndex -= 1;
+      if (routeIndex === 0) {
+        setDisabledBack(true);
+      }
+      setDisabledForward(false);
+    } else {
+      routeIndex += 1;
+      if (routeIndex === routeStack.length - 1) {
+        setDisabledForward(true);
+      }
+      setDisabledBack(false);
+    }
+
+    routeAction = RouteActions.NEW_ROUTE;
+    if (url === "/player/home") {
+      setDisabledHome(true);
+    } else {
+      setDisabledHome(false);
+    }
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", onRouteChanged);
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChanged);
+    };
+  });
+
   return (
     <div className="player-body-section">
       <div className="player-body-header">
