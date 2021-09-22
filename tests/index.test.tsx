75b23f86d9376c7cd70b20b040f8c5cc5d8f3f50
Mehmet Emin Yıldırım <mehmetcts@gmail.com>
2021-09-22 15:22:01 +0300

    fix tests

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../pages/index";
+import { Provider } from "react-redux";
+import store from "../app/store";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
 import Enzyme, { mount } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import fetch from "cross-fetch";
+
+const client = new ApolloClient({
+  ssrMode: typeof window === "undefined",
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+    credentials: "include",
+    fetch: fetch,
+  }),
+  cache: new InMemoryCache(),
+});
 
 Enzyme.configure({ adapter: new Adapter() });
-const wrapper = mount(<App />);
+const wrapper = mount(
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Provider>
+);
 
 test("renders Web Player link", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
+  );
   const linkElement = screen.getByText(/Web Player/i);
   expect(linkElement).toBeInTheDocument();
 });
