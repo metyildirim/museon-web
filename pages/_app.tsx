14de17044b6f2cf05213de220cf8197e437bb529
Mehmet Emin Yıldırım <mehmetcts@gmail.com>
2021-10-02 16:23:12 +0300

    update apolloClient crendentials

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ const { publicRuntimeConfig } = getConfig();
 
 const client = new ApolloClient({
   ssrMode: typeof window === "undefined",
+  credentials: "include",
   link: new HttpLink({
     uri: publicRuntimeConfig.graphqlEndpoint,
     credentials: "include",
